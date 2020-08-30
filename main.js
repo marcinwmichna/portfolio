09c0b5b9bf295eb1feb710a510b17f17551b1587
marcinwmichna <marcin.w.michna@gmail.com>
2020-08-30 02:03:24 +0200

    Heroku fetch to trigger database, few minor UI changes

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ let email = document.getElementById("email");
 let fadein = document.getElementById("fadein");
 let fadeout = document.getElementById("fadeout");
 
-const API_URL = "https://email-message.herokuapp.com/messages/";
+let herokuInit = document.getElementById("email");
+
+const API_URL = "http://localhost:5555/messages/";
+// const API_URL = "https://email-message.herokuapp.com/";
 cta.addEventListener("click", (event) => {
   event.preventDefault();
   if (this.email.value == null || this.email.value == "") {
@@ -16,13 +19,21 @@ cta.addEventListener("click", (event) => {
       }),
       headers: { "Content-Type": "application/json" },
     };
+    // console.log(addData);
     fetch(API_URL, addData).then((res) => {
       if (res.ok) {
         fadeout.classList.add("fadeout");
         fadein.classList.add("fadein");
       } else {
-        console.log("NOT succedded");
+        console.log("NOT succedded", res);
       }
     });
   }
+});
+
+herokuInit.addEventListener("click", (event) => {
+  event.preventDefault();
+  fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => data);
 });
